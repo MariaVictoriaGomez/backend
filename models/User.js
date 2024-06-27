57c7041bd73cf0751fd99d4c18f6cdd379b27f68
dmatia <diegovillalba.96@gmail.com>
2024-06-26 22:08:56 -0300

    Agrego clase user

---
+++ models/User.js
@@ -0,0 +1,8 @@
+export default class User{
+    constructor(id, username, password, usertype){
+        this.id = parseInt(id);
+        this.username = username;
+        this.password = password;
+        this.usertype = usertype;
+    }
+    }
