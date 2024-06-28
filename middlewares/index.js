996dfe1f0d28c954ef865a47d7bfcc8f85f87975
dmatia <diegovillalba.96@gmail.com>
2024-06-28 00:15:01 -0300

    Agrego index.js, errors y routes a middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+export { middlewares as filesMiddlerare } from "../files/files.middlewares.js";
+export { middlewares as routesMiddleware } from './routes.middlewares.js';
+import { middlewares as files } from "../files/files.middlewares.js";
+import { middlewares as routes } from "./routes.middlerares.js";
+import { middlewares as errors } from './errors.middlewares.js';
+
+export const middlerares = {
+    files,
+    routes,
+    errors
+}
