05da5d08fea7c5860252093f07e76d7fd2815f7e
unknown <mariavictoriagomez1988@gmail.com>
2024-06-28 22:40:12 -0300

    provincias js

--- middlewares/routes.middlerares.js
+++ middlewares/routes.middlerares.js
@@ -1,11 +1,7 @@
 // const checkRoute = (req, res, next) => {
 //     if(req.method === 'GET' && req.url.lenght > 1)
 //         return res.json({
-<<<<<<< HEAD
-//             error_code: 1 ,
-=======
 //             error_code: ,
->>>>>>> 7f60dacde0a00ade822413dd76c5ef68289e4dc6
 //             error_desc:'Ruta no implementada',
 //             endpoint: req.originalUrl,
 //             method: req.method
