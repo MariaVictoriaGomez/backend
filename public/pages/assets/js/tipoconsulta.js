e3ef229f6d1451e054e7b7ea42b11df6b832edad
dmatia <diegovillalba.96@gmail.com>
2024-07-01 14:21:54 -0300

    Estilos formulario consultas

--- public/pages/assets/js/tipoconsulta.js
+++ public/pages/assets/js/tipoconsulta.js
@@ -1,26 +1,26 @@
 async function cargarTiposConsulta() {
     try {
-      //  console.log("Estoy en cargar tipos consulta");
+        //  console.log("Estoy en cargar tipos consulta");
 
         const response = await fetch('/tipoconsulta');
         const tiposConsulta = await response.json();
-   //console.log(tiposConsulta);
+        //console.log(tiposConsulta);
 
         const tiposConsultaDiv = document.querySelector('.tipos-consulta');
 
         tiposConsulta.forEach(tipo => {
+            const div = document.createElement('div');
             const label = document.createElement('label');
             label.textContent = tipo.descripcion;
-
             const input = document.createElement('input');
             input.type = 'checkbox';
             input.name = 'tipoConsulta';
+            input.id = tipo.id;
             input.value = tipo.id;
 
-
-            tiposConsultaDiv.appendChild(input);
-            tiposConsultaDiv.appendChild(label);
-
+            div.appendChild(input);
+            div.appendChild(label);
+            tiposConsultaDiv.appendChild(div);
         });
     } catch (error) {
         console.error('Error al cargar los tipos de consulta:', error);
