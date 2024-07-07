4cb13c033a8f9f875469befc17c97f2b51d427e1
dmatia <diegovillalba.96@gmail.com>
2024-07-06 21:58:49 -0300

    Cambio conexion a DB Online

--- db/connections/Mysql.config.js
+++ db/connections/Mysql.config.js
@@ -7,14 +7,14 @@ export default class Mysql {
         
          this.connection = mysql.createConnection({
         //this.connection = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            password: '1234',
-            database: 'tiendacafe'
-            // host: process.env.HOST_DB,
-            // user: process.env.USER_DB,
-            // password: process.env.PASS_DB,
-            // database: process.env.DB
+            // host: 'localhost',
+            // user: 'root',
+            // password: '1234',
+            // database: 'tiendacafe'
+            host: process.env.HOST_DB,
+            user: process.env.USER_DB,
+            password: process.env.PASS_DB,
+            database: process.env.DB
         })
         this.tryConnection();
     }
